e031b8d31c872d797eced3d13661a362808c708b
Abhishek Shrestha <abhishek.shr2000@gmail.com>
2022-08-05 18:22:56 +0800

    Add serializer validation

--- books/book_api/serializer.py
+++ books/book_api/serializer.py
@@ -2,6 +2,7 @@
 
 from rest_framework import serializers
 from book_api.models import Book
+from django.forms import ValidationError
 
 
 class BookSerializer(serializers.ModelSerializer):
@@ -9,3 +10,13 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = ["id", "title", "number_of_pages", "publish_date", "quantity"]
         read_only_fields = ["id"]
+
+    def validate_title(self, value):
+        if value == "Diet Coke":
+            raise ValidationError("No Diet Coke Please.")
+        return value
+
+    def validate(self, data):
+        if data["number_of_pages"] < 100:
+            raise ValidationError("Too few pages. Big Books only.")
+        return data
