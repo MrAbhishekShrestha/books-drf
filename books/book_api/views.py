2c1069f80e2d27b193b608969b834afb44fa5fd6
Abhishek Shrestha <abhishek.shr2000@gmail.com>
2022-08-05 12:44:28 +0800

    use serializer and api_view to refactor views.py

--- books/book_api/views.py
+++ books/book_api/views.py
@@ -1,13 +1,14 @@
 """Books Views."""
 from book_api.models import Book
-from django.http import JsonResponse
+from book_api.serializer import BookSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
 
+@api_view(['GET'])
 def book_list(request):
     books = Book.objects.all()  # complex data
-    books_python = list(books.values())  # python ds
-    return JsonResponse({
-        'books': books_python
-    })
+    serializer = BookSerializer(books, many=True)  # Json data
+    return Response(serializer.data)
