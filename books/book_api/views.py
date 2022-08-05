1214e49e825f67dfdef805cd362f72d49e6a09d8
Abhishek Shrestha <abhishek.shr2000@gmail.com>
2022-08-05 12:28:48 +0800

    create the book model, view and add book urls to project

--- books/book_api/views.py
+++ books/book_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+"""Books Views."""
+from book_api.models import Book
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def book_list(request):
+    books = Book.objects.all()  # complex data
+    books_python = list(books.values())  # python ds
+    return JsonResponse({
+        'books': books_python
+    })
